52efd82aa3d3a26dffe8367d7861239f4aea0701
sonam <jha994048@gmail.com>
2021-09-25 17:05:20 +0530

    first commit

---
+++ src/main/java/com/ciber/qa/features/LoginStepDefination.java
@@ -0,0 +1,46 @@
+package com.ciber.qa.features;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefination {
+	@Given("^I want to write a step with precondition$")
+    public void i_want_to_write_a_step_with_precondition() throws Throwable {
+        
+    }
+
+    @When("^I complete action$")
+    public void i_complete_action() throws Throwable {
+        
+    }
+
+    @Then("^I validate the outcomes$")
+    public void i_validate_the_outcomes() throws Throwable {
+        
+    }
+
+    @And("^some other precondition$")
+    public void some_other_precondition() throws Throwable {
+        
+    }
+
+    @And("^some other action$")
+    public void some_other_action() throws Throwable {
+        
+    }
+
+    @And("^yet another action$")
+    public void yet_another_action() throws Throwable {
+        
+    }
+
+    @And("^check more outcomes$")
+    public void check_more_outcomes() throws Throwable {
+        
+    }
+
+}
+
+
